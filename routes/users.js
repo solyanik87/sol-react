7481063d3ebf787b2cc89a8b2aaa5fd1f08aad40
solyanik <km40087@gmail.com>
2024-10-30 20:40:37 +0200

    add server and client

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,15 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const {login, register, current} = require('../controllers/users');
+const auth = require('../middleware/auth');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+// /api/user/login 
+router.post('/login', login);
+
+// /api/user/register 
+router.post('/register', register);
+
+// /api/user/current 
+router.get('/current', auth, current);
 
 module.exports = router;
