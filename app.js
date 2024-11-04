9f1d6278ea2695a44d1058e4ef2430c680e00e47
solyanik <km40087@gmail.com>
2024-11-04 12:16:10 +0200

    fix corse

--- app.js
+++ app.js
@@ -7,6 +7,13 @@ require('dotenv').config();
 
 
 const app = express();
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Methods', '*')
+    res.setHeader('Access-Control-Allow-Headers', '*')
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    next()
+  })
 
 app.use(logger('dev'));
 app.use(express.json());
